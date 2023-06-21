36a3d7adde95e546b47e7cc9d3be50e51c57b38d
Jawad Raza <jawadraza8372@gmail.com>
2023-06-21 12:25:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,40 +13,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 app.get("/", (req, res) => {
-	res.render("index");
+  res.render("index");
 });
-app.post("/price_set", async (req, res) => {
-	const { price } = req.body;
-	try {
-		const session = await stripe.checkout.sessions.create({
-			payment_method_types: ["card"],
-			mode: "payment",
-			line_items: [
-				{
-					price_data: {
-						currency: `${process.env.currency}`,
-						product_data: {
-							name: "Custom Services",
-						},
-						unit_amount: parseInt(price) * 100,
-					},
-					quantity: 1,
-				},
-			],
-
-			success_url: `${process.env.api_url}/success`,
-			cancel_url: `${process.env.api_url}/cancel`,
-		});
-		res.redirect(session.url);
-	} catch (e) {
-		res.status(500).json({ error: e.message });
-	}
+app.get("/price_set/:price", async (req, res) => {
+  const { price } = req.params;
+  console.log(price);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+	  currency: `${process.env.currency}`,
+            product_data: {
+              name: "Custom Services",
+            },
+            unit_amount: price,
+          },
+          quantity: 1,
+        },
+      ],
+		success_url: `${process.env.api_url}/success`,
+		cancel_url: `${process.env.api_url}/cancel`,
+    });
+    res.redirect(session.url);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 app.get("/cancel", (req, res) => {
-	res.render(`cancel`);
+	res.json({ msg: "Cancel" });
 });
 app.get("/success", (req, res) => {
-	res.render(`success`);
+	
+	res.json({ msg: "success" });
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
